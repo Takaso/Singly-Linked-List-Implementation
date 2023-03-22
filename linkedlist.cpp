1110972103b8c09525944c982ed5ea6a876b6ff6
Takaso Nogokui <70639611+Takaso@users.noreply.github.com>
2023-03-22 13:46:37 +0100

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
+#include <time.h>
 
 struct Nodo {
-   int data;
-   struct Nodo *prossimo_el;
+    int data;
+    struct Nodo *prossimo_el;
 };
-
-struct Nodo* inizio = nullptr;
+struct Nodo* inizio = nullptr; // Testa del nodo
 void inserisci(int d) {
-   struct Nodo* n_nodo = (struct Nodo*) malloc(sizeof(struct Nodo));
-   n_nodo->data = d; n_nodo->prossimo_el = inizio; inizio = n_nodo;
+    struct Nodo* n_nodo = (struct Nodo*) malloc(sizeof(struct Nodo)); // Crea spazio nello stack per ogni nodo
+    n_nodo->data = d; n_nodo->prossimo_el = inizio; inizio = n_nodo;
 }
 void mostra() {
-   struct Nodo* ptr;
-   ptr = inizio;
-   while (ptr != nullptr) { // Per ogni elemento della lista fino a che non raggiunge il terminatore NULL
-      std::cout<< ptr->data <<" ";
-      ptr = ptr->prossimo_el;
-   }
+    struct Nodo* ptr; ptr = inizio;
+    while (ptr != nullptr) { // Per ogni elemento della lista fino a che non raggiunge il terminatore NULL
+        std::cout<<ptr->data<<"\n";
+        ptr = ptr->prossimo_el;
+    }
 }
 int main() {
-   for (unsigned int i=0; i<10; i++) {
-       inserisci(i);
-   }
-   mostra();
-   return 0;
+    srand(time(NULL));
+    for (unsigned int _=0; _<69; _++) { // Inserisce 69 valori randomici nel nodo
+        inserisci(rand());
+    }
+    mostra();
+    return 0;
 }
